cea7dab0e63498ad5e5d59fbf3064374ecf11203
Dmitry Maximyuk <d_maksimyk@vk.com>
2025-08-16 15:55:05 +0500

    feat: add telegram sign and bridge

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,9 @@ import "./styles/main.sass";
 
 import { render } from "solid-js/web";
 import { i18n } from "@/shared/lib";
+import { retrieveRawInitData, postEvent } from "@telegram-apps/bridge";
+
+import { tgGenerateSign } from "@/shared/lib";
 
 import App from "./App";
 
@@ -12,6 +15,17 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
     "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?",
   );
+}
+
+/* init data */
+postEvent("iframe_ready", {
+  reload_supported: true,
+});
+
+/* get start params */
+const preparedAuthData = tgGenerateSign(retrieveRawInitData());
+if (preparedAuthData) {
+  // console.log(preparedAuthData);
 }
 
 i18n.parseLocale();
---
+++ src/shared/lib/signature/index.ts
@@ -0,0 +1,25 @@
+export const tgGenerateSign = (
+  tgSecret: undefined | string,
+): undefined | string => {
+  if (!tgSecret) {
+    return;
+  }
+
+  const params = new URLSearchParams(tgSecret);
+
+  const authData = {
+    d: Array.from(params.entries())
+      .filter(([key]) => key !== "hash")
+      .sort(([a], [b]) => a.localeCompare(b))
+      .map(([k, v]) => `${k}=${v}`)
+      .join("\n"),
+    u: {
+      auth_date: +(params.get("auth_date") || ""),
+      user: JSON.parse(params.get("user") || ""),
+      query_id: params.get("query_id") || "",
+    },
+    h: params.get("hash") || "",
+  };
+
+  return btoa(JSON.stringify(authData));
+};
